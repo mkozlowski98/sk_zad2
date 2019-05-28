2f3f6c85386e2c3dcc87a601b49d83cb20313b2e
mkozlowski98 <maciekkozlowski98@gmail.com>
2019-05-29 00:14:09 +0200

    useless commit

--- cmake-build-debug/CMakeFiles/zad2.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/zad2.dir/DependInfo.cmake
@@ -4,8 +4,8 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/maciek/studies/4_sem/sik/zad2/netstore-client.c" "/home/maciek/studies/4_sem/sik/zad2/cmake-build-debug/CMakeFiles/zad2.dir/netstore-client.c.o"
-  "/home/maciek/studies/4_sem/sik/zad2/netstore-server.c" "/home/maciek/studies/4_sem/sik/zad2/cmake-build-debug/CMakeFiles/zad2.dir/netstore-server.c.o"
+  "/home/koksu/studies/4_sem/sik/zad2/netstore-client.c" "/home/koksu/studies/4_sem/sik/zad2/cmake-build-debug/CMakeFiles/zad2.dir/netstore-client.c.o"
+  "/home/koksu/studies/4_sem/sik/zad2/netstore-server.c" "/home/koksu/studies/4_sem/sik/zad2/cmake-build-debug/CMakeFiles/zad2.dir/netstore-server.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
